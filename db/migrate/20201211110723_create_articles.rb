d3184dee140ecd2ec63a45417d6faa5e8f1dc836
nazifberat <nberat5@outlook.com>
2020-12-11 14:07:58 +0300

    all migrations were done

---
+++ db/migrate/20201211110723_create_articles.rb
@@ -0,0 +1,6 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_195902) do
+ActiveRecord::Schema.define(version: 2020_12_10_155501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
